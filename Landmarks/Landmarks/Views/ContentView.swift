09dab854af1dff632fc7f523794c982549e699bd
FreddyQuispe <fquispe@smartsolutions.com.pe>
2021-01-31 01:22:28 -0500

    Building Lists and Navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Freddy Alexander Quispe Torres on 30/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
